c042dae251bf790492e41a1d7b23ae96e31f05ad
Arman Arjmand <128024362+Tundi007@users.noreply.github.com>
2024-07-14 09:04:28 +0330

    changes

--- src/CentralBank.java
+++ src/CentralBank.java
@@ -1,25 +1,19 @@
 import java.time.LocalDate;
-import java.time.Period;
 
 public class CentralBank implements IBank
 {
 
-    public int userSession_Int;
+    String bankDefinedAccountNumber_String;
 
-    // public abstract  CentralBank(int userSession_Int)
-    // {
+    String bankDefinedCardNumber_String;
 
-    //     this.userSession_Int = userSession_Int;
+    private String accountNumber_String = "";
 
-    // }
-
-    private String accountNumber_String;
-
-    private String cardNumber_String;
+    private String cardNumber_String = "";
 
     private LocalDate cardDate_LocalDate;
 
-    private String cvv2_String;
+    private String cvv2_String = "";
 
     @Override
     public String GetAccountNumber_Method()
@@ -75,6 +69,9 @@ public class CentralBank implements IBank
         if(Checkpoint.AccountNumberValidation_Method(accountNumber_String) == 1)
             return 1;
 
+        if(Checkpoint.BankDefined.AccountNumberValidation_Method(accountNumber_String) == 1)
+            return 1;
+
         this.accountNumber_String = accountNumber_String;
 
         return 0;
@@ -88,6 +85,9 @@ public class CentralBank implements IBank
         cardNumber_String = cardNumber_String.trim();
 
         if(Checkpoint.CardNumberValidation_Method(cardNumber_String) == 1)
+            return 1;
+
+        if(Checkpoint.BankDefined.CardNumberValidation_Method(cardNumber_String) == 1)
             return 1;
 
         this.cardNumber_String = cardNumber_String;
@@ -114,81 +114,39 @@ public class CentralBank implements IBank
     }
 
     @Override
-    public int SetCvv2_Method(String cardNumber_String)
+    public int SetCvv2_Method(String cvv2_String)
     {
 
-        cardNumber_String = cardNumber_String.trim();
+        cvv2_String = cvv2_String.trim();
 
-        if(Checkpoint.Cvv2Validation_Method(cardNumber_String) == 1)
+        if(Checkpoint.Cvv2Validation_Method(cvv2_String) == 1)
             return 1;
 
-        this.cardNumber_String = cardNumber_String;
+        this.cvv2_String = cvv2_String;
 
         return 0;
 
     }
 
-    public static class Checkpoint
+    private static class Checkpoint extends IBank.Checkpoint
     {
 
-        private static int AccountNumberValidation_Method(String accountNumber_String)
+        public static class BankDefined
         {
 
-            if(accountNumber_String.length() != 10)
-                return 1;
-
-            return 0;
-        
-        }
-
-        private static int CardNumberValidation_Method(String cardNumber_String)
-        {
-
-            if(cardNumber_String.length() != 16)
-                return 1;
-
-            if(!String.format("%1$c%2$c%3$c%4$c",
-                cardNumber_String.charAt(0),
-                cardNumber_String.charAt(1),
-                cardNumber_String.charAt(2),
-                cardNumber_String.charAt(3)).equals("5051"))
-                return 1;
-
-            return 0;
-
-        }
-
-        public abstract static class BankDefined
-        {
-
-            public abstract int BankDefinedAccountNumberValidation_Method(String accountNumber_String);
+            protected static int AccountNumberValidation_Method(String accountNumber_String)
+            {
+            
+                return 0;
+            
+            }
     
-            public abstract int BankDefinedCardNumberValidation_Method(String cardNumber_String);
-        
-        }
-
-        private static int CardDateValidation_Method(LocalDate tempCardDate_LocalDate)
-        {
-
-            Period CardDate_Period;
-
-            if((Period.of(5, 0, 0).minus(CardDate_Period = Period.between(
-                tempCardDate_LocalDate,
-                LocalDate.now()))).isNegative() |
-                CardDate_Period.isNegative())
-                return 1;
-
-            return 0;
+            protected static int CardNumberValidation_Method(String cardNumber_String)
+            {
+                
+                return 0;
             
-        }
-
-        private static int Cvv2Validation_Method(String cvv2_String)
-        {
-
-            if(cvv2_String.length() != 3 & cvv2_String.length() != 4)
-                return 1;
-            
-            return 0;
+            }
         
         }
 
--- src/IBank.java
+++ src/IBank.java
@@ -1,32 +1,80 @@
+
+import java.time.LocalDate;
+import java.time.Period;
+
 interface  IBank
 {
 
-    public String GetAccountNumber_Method();
+    String GetAccountNumber_Method();
 
-    public String GetCardNumber_Method();
+    String GetCardNumber_Method();
 
-    public String GetCardDate_Method();
+    String GetCardDate_Method();
 
-    public String GetCvv2_Method();
+    String GetCvv2_Method();
 
-    public int SetAccountNumber_Method(String accountNumber_String);
+    int SetAccountNumber_Method(String accountNumber_String);
 
-    public int SetCardNumber_Method(String cardNumber_String);
+    int SetCardNumber_Method(String cardNumber_String);
 
-    public int SetCardDate_Method(String cardDate_String);
+    int SetCardDate_Method(String cardDate_String);
 
-    public int SetCvv2_Method(String cvv2_String);
+    int SetCvv2_Method(String cvv2_String);
     
-    public static abstract class Checkpoint
+    static class Checkpoint
     {
 
-        public abstract int AccountNumberValidation_Method(String accountNumber_String);
+        protected static int AccountNumberValidation_Method(String accountNumber_String)
+        {
 
-        public abstract int CardNumberValidation_Method(String cardNumber_String);
+            if(accountNumber_String.length() != 10)
+                return 1;
 
-        public abstract int CardDateValidation_Method(String cardDate_String);
+            return 0;
+        
+        }
 
-        public abstract int Cvv2Validation_Method(String cvv2_String);
+        protected static int CardNumberValidation_Method(String cardNumber_String)
+        {
+
+            if(cardNumber_String.length() != 16)
+                return 1;
+
+            if(!String.format("%1$c%2$c%3$c%4$c",
+                cardNumber_String.charAt(0),
+                cardNumber_String.charAt(1),
+                cardNumber_String.charAt(2),
+                cardNumber_String.charAt(3)).equals("5051"))
+                return 1;
+
+            return 0;
+
+        }
+
+        protected static int CardDateValidation_Method(LocalDate tempCardDate_LocalDate)
+        {
+
+            Period CardDate_Period;
+
+            if((Period.of(5, 0, 0).minus(CardDate_Period = Period.between(
+                tempCardDate_LocalDate,
+                LocalDate.now()))).isNegative() |
+                CardDate_Period.isNegative())
+                return 1;
+
+            return 0;
+            
+        }
+
+        protected static int Cvv2Validation_Method(String cvv2_String)
+        {
+
+            if(cvv2_String.length() != 3 & cvv2_String.length() != 4)
+                return 1;
+            
+            return 0;
+        
+        }
 
     }
     
